c4318c534657ac3c0f2b007d29ecf9111091c042
sande <sandetekmi@gmail.com>
2023-04-29 01:19:22 +0600

    интерфейс пен ғарыш кемесі және оны х осі бойынша қозғалттық

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import org.lwjgl.*;
+import javax.swing.*;
+import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         //Ғарыштық қорғаушылар
@@ -6,5 +8,51 @@ public class Main {
         //Ойынның мақсаты:ғарыш кемесімен алдына келген жауларды атып, өз кеңістігіңді қорғау.
 
         //Ойын ережесі:уақыт біткенше жаулардың бәрін атып сындырсақ,ұтыс табамыз.Егер бір жауды өткізіп алсақ , жеңіліс табамыз
+
+            JFrame frame = new JFrame("Spaceship Interface");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(500, 500);
+
+            SpaceshipPanel panel = new SpaceshipPanel();
+
+            frame.add(panel);
+            frame.setVisible(true);
+    }
+}
+
+class SpaceshipPanel extends JPanel {
+    Spaceship spaceship = new Spaceship(50, 50);
+
+    public SpaceshipPanel() {
+        Timer timer = new Timer(10, e -> {
+            spaceship.move();
+            repaint();
+        });
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        spaceship.draw(g);
+    }
+}
+
+class Spaceship {
+    private int x;
+    private int y;
+
+    public Spaceship(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move() {
+        x += 5;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, 50, 20);
     }
 }
