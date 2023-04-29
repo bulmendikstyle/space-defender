257ca995b8c5e950dd9913fdb97d810f8b1b6b52
sande <sandetekmi@gmail.com>
2023-04-29 09:39:54 +0600

    интерфейс пен ғарыш кемесі және оны х осі бойынша тоқтаусыз оңға солға қозғалттық және кемені ататын еттік

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 import org.lwjgl.*;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //Ғарыштық қорғаушылар
+        JFrame frame = new JFrame("Spaceship Interface");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
 
-        //Ойынның мақсаты:ғарыш кемесімен алдына келген жауларды атып, өз кеңістігіңді қорғау.
+        SpaceshipPanel panel = new SpaceshipPanel();
 
-        //Ойын ережесі:уақыт біткенше жаулардың бәрін атып сындырсақ,ұтыс табамыз.Егер бір жауды өткізіп алсақ , жеңіліс табамыз
-
-            JFrame frame = new JFrame("Spaceship Interface");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(500, 500);
-
-            SpaceshipPanel panel = new SpaceshipPanel();
-
-            frame.add(panel);
-            frame.setVisible(true);
+        frame.add(panel);
+        frame.setVisible(true);
     }
 }
 
@@ -24,19 +21,43 @@ class SpaceshipPanel extends JPanel {
     private int panelWidth = 500;
 
     Spaceship spaceship = new Spaceship(50, 50, panelWidth);
+    List<Bullet> bullets = new ArrayList<>();
 
     public SpaceshipPanel() {
         Timer timer = new Timer(10, e -> {
             spaceship.move();
+            for (Bullet bullet : bullets) {
+                bullet.move();
+            }
+            checkCollisions();
             repaint();
         });
         timer.start();
+
+        Timer shootingTimer = new Timer(500, e -> {
+            Bullet newBullet = new Bullet(spaceship.getX() + 25, spaceship.getY(), panelWidth);
+            bullets.add(newBullet);
+        });
+        shootingTimer.start();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         spaceship.draw(g);
+        for (Bullet bullet : bullets) {
+            bullet.draw(g);
+        }
+    }
+
+    private void checkCollisions() {
+        for (int i = 0; i < bullets.size(); i++) {
+            Bullet bullet = bullets.get(i);
+            if (spaceship.intersects(bullet)) {
+                bullets.remove(i);
+                i--;
+            }
+        }
     }
 }
 
@@ -64,5 +85,51 @@ class Spaceship {
     public void draw(Graphics g) {
         g.setColor(Color.RED);
         g.fillRect(x, y, 50, 20);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean intersects(Bullet bullet) {
+        Rectangle spaceshipRect = new Rectangle(x, y, 50, 20);
+        Rectangle bulletRect = new Rectangle(bullet.getX(), bullet.getY(), 5, 10);
+        return spaceshipRect.intersects(bulletRect);
+    }
+}
+
+class Bullet {
+    private int x;
+    private int y;
+    private int panelWidth;
+
+    public Bullet(int x, int y, int panelWidth) {
+        this.x = x;
+        this.y = y;
+        this.panelWidth = panelWidth;
+    }
+
+    public void move() {
+        y -= 10;
+        if (y < 0) {
+            y = 500;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillRect(x, y, 5, 10);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
